bc468efd12d10fb5c0638feb9464d4eb8d43e8a8
tsolomahase <mahase@iuj.ac.jp>
2020-10-07 09:59:48 +0000

    fixed the router

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   root "users#new"
-  get 'sessions/new'
   resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   resources :feeds do
     collection do
       post :comfirm
+      patch :confirm
       end
     end
   end
